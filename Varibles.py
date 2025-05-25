4cc6ae2b3435c8f7a72560593c3cefdeb76304e7
Aditya <adi465996@gmail.com>
2025-05-25 20:55:17 +0530

    Added info about String

--- Varibles.py
+++ Varibles.py
@@ -8,3 +8,10 @@
 7.Tuples(Ordered Immutable sequence)
 8.Sets(Unordered collection of Unique object)
 */
+//Varibles data type decleration is not required in python
+//Python starts counting from 0 and in negative it goes from right to left
+myString = "Hello World" //Strings
+mystring = [::2] // 2 is step size for jumping
+mystring = [::-1] // Reversing the Strings
+x = myString
+
