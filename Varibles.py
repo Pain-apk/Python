54bd60774bb426b5291451905ec38b8e9f8e269e
Aditya <adi465996@gmail.com>
2025-05-25 20:40:19 +0530

    Added comments

--- Varibles.py
+++ Varibles.py
@@ -1,1 +1,10 @@
-/* 
+/* There are 9 types of varibles i python
+1.Integers
+2.Float
+3.Strings
+4.Boolean
+5.Lists
+6.Dictionaries(Key Value pairs)
+7.Tuples(Ordered Immutable sequence)
+8.Sets(Unordered collection of Unique object)
+*/
