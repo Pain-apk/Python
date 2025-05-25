7e6dbfdfb82fc652c9b2cdb4b54ab4840dc2e56e
Aditya <adi465996@gmail.com>
2025-05-25 22:00:22 +0530

    Add examples of string interpolation in Python

---
+++ String_Interpolation.py
@@ -0,0 +1,12 @@
+/* There are two types of String Interpolation in python
+1. f-Strings (formatted string literals)
+2. str.format() method
+*/
+# f-strings can be used to embed experssion inside string literals, using curly braces '{}'
+name = "Aditya"
+age = 20
+print(f"My name is {name} and I am {age} years old.")
+# str.format() method can be used to format strings
+print("My name is {} and I am {} years old.".format(name, age))
+# You can also use the % foperator for String Interpolation
+print("My name is %s and I am %d years old." % (name, age))
