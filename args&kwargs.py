90059f4e17e9f3c24dca77346eaaa10cd62f7b7b
Aditya <adi465996@gmail.com>
2025-05-27 12:03:19 +0530

    Worked on kwargs

--- args&kwargs.py
+++ args&kwargs.py
@@ -1,13 +1,24 @@
-/*
-In this code we will be understanding *args and **kwargs in Python.
-*args allows you to pass a variblr number of positional arguments to a function.
-**kwargs allows you to pass a varuable number of keyword arguments to a function. 
-*/
+# In this code we will be understanding *args and **kwargs in Python.
+# *args allows you to pass a variable number of positional arguments to a function.
+#  **kwargs allows you to pass a variable number of keyword arguments to a function.
+
 def add(*args):
+    for item in args:
+        print(item)
     return sum(args)
-    add(1,2,3,4,5)
-    print(add(1,2,3,4,5))
-for item in args:
-    print(item)
+
+print(add(1, 2, 3, 4, 5))
+
+def myfunc(**kwargs):
+    print(kwargs)
+    if 'fruit' in kwargs:
+            print("My favourite fruit is {}".format(kwargs['fruit']))
+    else:
+            print("No fruit found")
 
 
+myfunc(fruit='apple', veggie='lettuce', jelly='grape')
+
+def myfunc2(*args, **kwargs):
+      print("I would like {} {}".format(args[0], kwargs['food']))
+myfunc2(1,2, 3, food = "sandwich")
