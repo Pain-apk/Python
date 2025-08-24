8585b2d6fba3356ab79ee34e6e3040ecbee3107a
Aditya <adi465996@gmail.com>
2025-08-24 17:34:21 +0530

    added lists

---
+++ datastructures.py
@@ -0,0 +1,5 @@
+#Data Structures in Python
+#1)Lists Ordered Sequence of Objects
+myList = ['One', 'two' ,'three']
+#It is same as string but the only difference 
+# is we can add or remove objects in between        
