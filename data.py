e3f38d02d707f4d0e69f4f312502f42a0dda7451
Aditya <adi465996@gmail.com>
2026-01-18 22:39:37 +0530

    Adding data types

--- data.py
+++ data.py
@@ -2,3 +2,9 @@
 #1)Lists Ordered Sequence of Objects
 myList = ['One', 'two' ,'three']
 
+# 2) Integers Whole Numbers with int as type and float for decimal numbers
+myInt =7
+myfloat = 7.5
+# 3) Strings sequence of characters
+mystring = "Yo python"
+
