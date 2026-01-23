0a9db33acd2af1a23d9290ea0691e4e5494acc36
Aditya <adi465996@gmail.com>
2026-01-23 22:33:07 +0530

    Added Strings.py to look deeper in Strings

--- Strings.py
+++ Strings.py
@@ -1,1 +1,10 @@
 #Sequence of characters, using the single quote or double quote
+#String and it's Operations
+mystring = "python is cool"
+#Returns values from 0 to 5
+print(mystring[0:5])
+#Returns with skipping one letter
+print(mystring[::2])
+#Reversing the String
+print(mystring[::-1])
+
