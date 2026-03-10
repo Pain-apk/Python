c8f767a8443de8c1f7ca5eb71bb0de86c01d21b3
Aditya <adi465996@gmail.com>
2026-03-10 10:48:29 +0530

    Added type castiing

--- Varibles.py
+++ Varibles.py
@@ -9,8 +9,15 @@
 #8.Sets(Unordered collection of Unique object)
 #Varibles data type decleration is not required in python
 #Python starts counting from 0 and in negative it goes from right to left
-myString = "Hello World" //Strings
-mystring = [::2] // 2 is step size for jumping
-mystring = [::-1] // Reversing the Strings
+myString = "Hello World" #Strings
+mystring = myString[::2] # 2 is step size for jumping
+mystring = myString[::-1] #/ Reversing the Strings
 x = myString
-
+#Typecasting is the process of converting one data type to another
+x = str(3) # converting integer to string
+val1 = input("Enter Val1")
+val2 = input("Enter Val2")
+print(val1 + val2) # This will concatenate the two strings
+val1 = int(input("Enter Val1"))
+val2 = int(input("Enter Val2"))
+print(val1 + val2) # This will add the two integers    
